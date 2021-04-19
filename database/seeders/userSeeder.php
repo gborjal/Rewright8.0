9648e8f1e4d1f086b1bbe7a7062fe27baaeead10
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-04-19 21:45:49 +0800

    update

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 use App\Models\discussion_notif;
 use App\Models\discussion_comment;
--- database/seeders/userTypesSeeder.php
+++ database/seeders/userTypesSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 use App\Models\user_types;
 
