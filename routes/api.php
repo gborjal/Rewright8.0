af92f7cfaf435f97dfe3d773f35412f5b8ca793e
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-04-25 21:21:39 +0800

    revert

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});*/
-Route::get('/demo-url',  function  (Request $request)  {
-   return response()->json(['Laravel 8 CORS Demo']);
 });
