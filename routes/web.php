78ca6f559c3e8e83118f91f4d6427d75a63acab5
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-04-28 23:39:52 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -87,37 +87,39 @@ Route::prefix('/tasks')->group(function () {
 	//Route::get('info', [taskController::class,'getTask']);
 
 });
-Route::prefix('/post')->group(['middleware' => ['cors']],function () {
-	Route::prefix('discussion')->group(function () {
-		Route::post('thread', [discussionController::class,'postDiscussion'])->name('postDiscussion');	//ajax request
-		//Route::get('thread', [discussionController::class,'postDiscussion']);
-		Route::post('comment', [discussionController::class,'postDiscussionComment'])->name('postDiscussionComment');	//ajax request
-		//Route::get('comment', [discussionController::class,'postDiscussionComment']);
-		Route::post('comment/cnt', [discussionController::class,'postDiscussionCommentCnt'])->name('postDiscussionCommentCnt');	//ajax request
-		//Route::get('comment/cnt', [discussionController::class,'postDiscussionCommentCnt']);
-		Route::post('notifs/cnt', [discussionController::class,'postDiscussionNotifsCnt'])->name('postDiscussionNotifsCnt');	//ajax request
-		//Route::get('notifs/cnt', [discussionController::class,'postDiscussionNotifsCnt']);
+Route::middleware(['web', 'cors'])->group(function () {
+	Route::prefix('/post')->group(function () {
+		Route::prefix('discussion')->group(function () {
+			Route::post('thread', [discussionController::class,'postDiscussion'])->name('postDiscussion');	//ajax request
+			//Route::get('thread', [discussionController::class,'postDiscussion']);
+			Route::post('comment', [discussionController::class,'postDiscussionComment'])->name('postDiscussionComment');	//ajax request
+			//Route::get('comment', [discussionController::class,'postDiscussionComment']);
+			Route::post('comment/cnt', [discussionController::class,'postDiscussionCommentCnt'])->name('postDiscussionCommentCnt');	//ajax request
+			//Route::get('comment/cnt', [discussionController::class,'postDiscussionCommentCnt']);
+			Route::post('notifs/cnt', [discussionController::class,'postDiscussionNotifsCnt'])->name('postDiscussionNotifsCnt');	//ajax request
+			//Route::get('notifs/cnt', [discussionController::class,'postDiscussionNotifsCnt']);
+		});
+		Route::prefix('task')->group(function () {
+			Route::post('item', [taskController::class,'postTask'])->name('postTask');	//ajax 
+			//Route::get('item', [taskController::class,'postTask']);
+			Route::post('patient', [taskController::class,'postTaskPatient'])->name('postTaskPatient');	//ajax 
+			//Route::get('patient', [taskController::class,'postTaskPatient']);
+			Route::post('patient/result', [taskController::class,'postTaskPatientResult'])->name('postTaskPatientResult');	//ajax 
+			Route::get('patient/result', [taskController::class,'postTaskPatientResult']);
+		});
+		Route::prefix('exerdata')->group(function () {
+			Route::post('/', [taskController::class,'postExerciseData'])->name('postExerciseData');	//ajax 
+			//Route::get('/', [taskController::class,'postExerciseData']);
+			Route::post('list', [taskController::class,'postExerciseDataList'])->name('postExerciseDataList');	//ajax 
+			//Route::get('list', [taskController::class,'postExerciseDataList']);
+			Route::post('adjustedScore', [taskController::class,'postAdjustedScore'])->name('postAdjustedScore');	//ajax 
+			Route::get('adjustedScore', [taskController::class,'postAdjustedScore']);
+		});
+		
+		Route::post('note', [notesController::class,'postNote'])->name('postNote');	//ajax 
+		//Route::get('note', [notesController::class,'postNote']);
+		
 	});
-	Route::prefix('task')->group(function () {
-		Route::post('item', [taskController::class,'postTask'])->name('postTask');	//ajax 
-		//Route::get('item', [taskController::class,'postTask']);
-		Route::post('patient', [taskController::class,'postTaskPatient'])->name('postTaskPatient');	//ajax 
-		//Route::get('patient', [taskController::class,'postTaskPatient']);
-		Route::post('patient/result', [taskController::class,'postTaskPatientResult'])->name('postTaskPatientResult');	//ajax 
-		Route::get('patient/result', [taskController::class,'postTaskPatientResult']);
-	});
-	Route::prefix('exerdata')->group(function () {
-		Route::post('/', [taskController::class,'postExerciseData'])->name('postExerciseData');	//ajax 
-		//Route::get('/', [taskController::class,'postExerciseData']);
-		Route::post('list', [taskController::class,'postExerciseDataList'])->name('postExerciseDataList');	//ajax 
-		//Route::get('list', [taskController::class,'postExerciseDataList']);
-		Route::post('adjustedScore', [taskController::class,'postAdjustedScore'])->name('postAdjustedScore');	//ajax 
-		Route::get('adjustedScore', [taskController::class,'postAdjustedScore']);
-	});
-	
-	Route::post('note', [notesController::class,'postNote'])->name('postNote');	//ajax 
-	//Route::get('note', [notesController::class,'postNote']);
-	
 });
 Route::prefix('/profile')->group(function () {
 	Route::get('image/{type}/{person}', [imagesController::class,'profilePicture']);
