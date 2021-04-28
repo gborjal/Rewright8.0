e491028119487b94c289a380ca9c0625d01394a3
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-04-28 23:17:58 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+*/
 Route::prefix('/post')->group(function () {
 	Route::prefix('discussion')->group(function () {
 		Route::post('thread', [discussionController::class,'postDiscussion'])->name('postDiscussion');	//ajax request
