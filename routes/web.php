ef9b0dd0f34deabdfe5723a864e33969d2da2979
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-05-01 14:54:37 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -118,8 +118,8 @@ Route::prefix('/tasks')->group(function () {
 			Route::get('adjustedScore', [taskController::class,'postAdjustedScore']);
 		});
 		Route::prefix('patient')->group(function () {
-			Route::post('list', [projectsController::class,'postPatientDataList'])->name('postExerciseDataList')->middleware('auth');	//ajax 
-			Route::get('list', [projectsController::class,'postPatientDataList'])->middleware('auth');
+			Route::post('list', [projectsController::class,'postPatientDataList'])->name('postExerciseDataList');	//ajax 
+			Route::get('list', [projectsController::class,'postPatientDataList']);
 		});
 		Route::post('note', [notesController::class,'postNote'])->name('postNote');	//ajax 
 		//Route::get('note', [notesController::class,'postNote']);
