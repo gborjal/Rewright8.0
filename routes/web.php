c2ae38a3f1c5409ab9337c5f0801ba53e9686e2c
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-05-13 15:25:09 +0800

    SPA sanctum
    authentication

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\userController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::middleware(['web', 'auth'])->group(function () {
+Route::middleware(['web', 'auth:api'])->group(function () {
     Route::get('/', [indexController::class,'index']);
     Route::prefix('auth')->group(function () {	
 		//Login routes
