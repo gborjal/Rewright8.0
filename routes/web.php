5a11ecd09a537651de74b78f8a6a0d353e0cbbc1
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-05-16 00:53:56 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,26 @@ Route::middleware(['web'])->group(function () {
 		//Admin Login Route
 		Route::post('admin/login',[AuthController::class,'showAdminLoginForm'])->name('showAdminLoginForm');
 	});
+	Route::prefix('auth')->group(function () {
+		Route::post('login', [AuthController::class,'login'])->name('login');
+		Route::get('logout',[userController::class,'getLogout'])->name('logout');
+
+		Route::prefix('admin')->group(function () {
+			Route::post('login', [AuthController::class,'loginAdmin'])->name('loginAdmin');
+			Route::get('logout',[userController::class,'getLogoutAdmin'])->name('logoutAdmin');
+		});
+		//Profile routes
+		Route::prefix('profile')->group(function () {
+			Route::post('save', [userController::class,'saveEditUserProfile'])->name('postEditProfile');	//ajax request
+			Route::get('edit/{code}',[userController::class,'editUserProfile1']);
+			Route::post('edit/{code}', [userController::class,'editUserProfile1'])->name('editProfile');
+		});
+		//Specialist search
+		Route::prefix('search')->group(function () {
+			//Route::get('auth/search/patient', 'userController@getPatientSrch');
+			Route::post('patient', [userController::class,'getPatientSrch'])->name('getPatientSrch');
+		});
+	});
 });
 
 // Route::middleware(['web'])->group(function () {
