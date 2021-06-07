ec1674425453a36557c35d4b82496368f5b2f1c9
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-06-08 01:22:48 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -26,10 +26,8 @@ use App\Http\Controllers\userController;
 Route::group(['middleware' => ['auth:sanctum']], function(){
 	Route::prefix('auth')->group(function () {
 		// Route::get('login', [AuthController::class,'login'])->name('login');
-		Route::post('login',function(){
-			echo "here";
-		})->name('login');
-		//Route::post('login', [AuthController::class,'login'])->name('login');
+
+		Route::post('login', [AuthController::class,'login'])->name('login');
 		//Route::get('logout',[userController::class,'getLogout'])->name('logout');
 
 		// Route::prefix('admin')->group(function () {
