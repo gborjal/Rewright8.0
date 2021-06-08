187f6c825ec58a7e179c5ef71350f4a5b7f986eb
xsephtion <josephborjaf@gmail.com>
2021-06-08 13:20:20 +0800

    Added name

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ use App\Http\Controllers\userController;
 Route::group(['middleware' => ['auth:sanctum']], function(){
 
 	Route::prefix('auth')->group(function () {;
-		Route::match(['get', 'post'], [AuthController::class,'login'])->name('login');
+		Route::match(['get', 'post'], 'login',[AuthController::class,'login'])->name('login');
 		Route::get('logout',[userController::class,'getLogout'])->name('logout');
 
 		// Route::prefix('admin')->group(function () {
