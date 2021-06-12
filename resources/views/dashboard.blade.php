0f450bf33027f68add4f52d36ec08548e1a96793
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-06-13 00:46:36 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 	
 	<header>
-		<nav class="top-nav">
+		<nav class="top-nav grey darken-4">
 			<div class="container">
-				<div class="nav-wrapper grey darken-4" >
+				<div class="nav-wrapper">
 					<a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">dashboard</i></a>			
 					<ul class="right hide-on-med-and-down right">
 						<li><a href="{!! route('logout') !!}">logout</a></li>
