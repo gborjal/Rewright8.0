c288f31cbc0d6fc6158ccd98613d2f27bb6f78ae
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-06-14 23:59:42 +0800

    Update functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -1005,13 +1005,10 @@ $(document).ready(function(){
 		menuWidth: 300, // Default is 240
 		edge: 'left', // Choose the horizontal origin
 		closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-		});
-	/*$('.button-collapse').sidenav({
-		menuWidth: 300, // Default is 240
-		edge: 'left', // Choose the horizontal origin
-		closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
 		}
-	);*/
+	);
+	$('.collapsible').collapsible();
+	$('.modal').modal();
 	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     /*
     $('.modal-trigger').leanModal({
