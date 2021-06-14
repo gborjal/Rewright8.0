1b46ef7fb7fb1a71de6e954bbb22bd4a07594b01
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-06-15 01:54:59 +0800

    Update index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,29 +1,7 @@
 @extends('index_master')
 
 @section('content')
-	@if($errors->any())
-		<script type="text/javascript">
-			@foreach($errors->all() as $error)
 
-			
-				var toastContent = "<span>{{ $error }}</span>";
-				M.toast({ 	html:toastContent,
-							displayLength:5000, 
-							classes:'red darken-4'
-						});
-			
-			@endforeach
-		</script>
-	@endif
-	@if(Session::get('error'))
-		<script type="text/javascript">
-			var toastContent = "<span>{{ Session::get('error') }}</span>";
-			M.toast({ 	html:toastContent,
-						displayLength:5000, 
-						classes:'red darken-4'
-					});
-		</script>
-	@endif
 	<div class="container">
 		<div class="row">
 			<div class="col s12 m6 l6 offset-m3 offset-l3">
@@ -56,4 +34,28 @@
 
 
 @stop
+@section('errors')
+	@if($errors->any())
+		<script type="text/javascript">
+			@foreach($errors->all() as $error)
 
+			
+				var toastContent = "<span>{{ $error }}</span>";
+				M.toast({ 	html:toastContent,
+							displayLength:5000, 
+							classes:'red darken-4'
+						});
+			
+			@endforeach
+		</script>
+	@endif
+	@if(Session::get('error'))
+		<script type="text/javascript">
+			var toastContent = "<span>{{ Session::get('error') }}</span>";
+			M.toast({ 	html:toastContent,
+						displayLength:5000, 
+						classes:'red darken-4'
+					});
+		</script>
+	@endif
+@stop
