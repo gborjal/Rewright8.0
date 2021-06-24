9c3eda488d987ce4011eba1536dcf431cab1f400
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-06-25 03:07:13 +0800

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -87,58 +87,59 @@ class userController extends Controller
      *
      * @return View
      */
-    public function editUserProfile1(Request $request,$code){
-        if($request->ajax() ){
-            if(Auth::user()->user_types === 0 || Auth::user()->user_types === 1){
-                $user_id = DB::table('users')
-                            ->select('id','email','activation_code as code')
-                            ->where('activation_code','=',$code)
-                            ->first();
-                $user_cnt = user_info::select('*')
-                                ->where('user_id','=',$user_id->id)
-                                ->count();
-                
-                if($user_cnt == 0){
-                    $user_info_id = DB::table('users_info')->insert([
-                        "user_id"=>$user_id->id,
-                        "profile"=>'1_profile_1.jpg',
-                        "banner"=>'1_banner_1.jpg',
-                        "first_name"=>'',
-                        "middle_name"=>'',
-                        "last_name"=>'',
-                        "suffix_name"=>NULL,
-                        "sex"=>NULL,
-                        "perm_address"=>NULL,
-                        "tempo_address"=>NULL,
-                        "office_address"=>NULL
-                    ]);
-                }
-                $user_info = user_info::select('profile as prof_pic',
-                                            'first_name as fname',
-                                            'middle_name as mname',
-                                            'last_name as lname',
-                                            'suffix_name as sname',
-                                            'sex',
-                                            'perm_address as p_add',
-                                            'tempo_address as t_add',
-                                            'office_address as o_add')
+    public function editUserProfile1($code){
+        
+        if(Auth::user()->user_types === 0 || Auth::user()->user_types === 1){
+            $user_id = DB::table('users')
+                        ->select('id','email','activation_code as code')
+                        ->where('activation_code','=',$code)
+                        ->first();
+            $user_cnt = user_info::select('*')
                             ->where('user_id','=',$user_id->id)
-                            ->first();
-                $user = array('prof_pic' => $user_info->prof_pic,
-                                'fname'  => $user_info->fname,
-                                'mname'  => $user_info->mname,
-                                'lname'  => $user_info->lname,
-                                'sname'  => $user_info->sname,
-                                'sex'    => $user_info->sex,
-                                'p_add'  => $user_info->p_add,
-                                't_add'  => $user_info->t_add,
-                                'o_add'  => $user_info->o_add,
-                                'email'  => $user_id->email,
-                                'code'   => $user_id->code);
-                $tokenResult = Auth::user()->createToken('authToken')->plainTextToken;
-                return view('profileEdit',['user_info'=>$user])
-                            ->with('authToken',$tokenResult);
+                            ->count();
+            
+            if($user_cnt == 0){
+                $user_info_id = DB::table('users_info')->insert([
+                    "user_id"=>$user_id->id,
+                    "profile"=>'1_profile_1.jpg',
+                    "banner"=>'1_banner_1.jpg',
+                    "first_name"=>'',
+                    "middle_name"=>'',
+                    "last_name"=>'',
+                    "suffix_name"=>NULL,
+                    "sex"=>NULL,
+                    "perm_address"=>NULL,
+                    "tempo_address"=>NULL,
+                    "office_address"=>NULL
+                ]);
             }
+            $user_info = user_info::select('profile as prof_pic',
+                                        'first_name as fname',
+                                        'middle_name as mname',
+                                        'last_name as lname',
+                                        'suffix_name as sname',
+                                        'sex',
+                                        'perm_address as p_add',
+                                        'tempo_address as t_add',
+                                        'office_address as o_add')
+                        ->where('user_id','=',$user_id->id)
+                        ->first();
+            $user = array('prof_pic' => $user_info->prof_pic,
+                            'fname'  => $user_info->fname,
+                            'mname'  => $user_info->mname,
+                            'lname'  => $user_info->lname,
+                            'sname'  => $user_info->sname,
+                            'sex'    => $user_info->sex,
+                            'p_add'  => $user_info->p_add,
+                            't_add'  => $user_info->t_add,
+                            'o_add'  => $user_info->o_add,
+                            'email'  => $user_id->email,
+                            'code'   => $user_id->code);
+            $tokenResult = Auth::user()->createToken('authToken')->plainTextToken;
+            return view('profileEdit',['user_info'=>$user])
+                        ->with('authToken',$tokenResult);
+        }else{
+            //view only redirect to another view
         }
     }
     
@@ -149,62 +150,62 @@ class userController extends Controller
      * @return Response
      */
     public function saveEditUserProfile(Request $request){
-        dd($request->all());
-        $response = [
-            'success'       => "",
-            'message'       => []
-        ];
-        $input = $request->only([
-            'h_email',
-            'h_code',
-            'fname',
-            'mname',
-            'lname',
-            'suffix_name',
-            'sex',
-            'perm_address',
-            'tempo_address',
-            'office_address'
-            ]);
-        $rules = array(
-            'h_email'       => 'required|email|max:255',
-            'h_code'        => 'required|string|max:8',
-            'fname'         => 'required|string',
-            'mname'         => 'required|string',
-            'lname'         => 'required|string',
-            'sex'           => 'required|string',
-            'perm_address'  => 'required|string|min:10'
-        );
+        if($request->ajax()){
+            $response = [
+                'success'       => "",
+                'message'       => []
+            ];
+            $input = $request->only([
+                'h_email',
+                'h_code',
+                'fname',
+                'mname',
+                'lname',
+                'suffix_name',
+                'sex',
+                'perm_address',
+                'tempo_address',
+                'office_address'
+                ]);
+            $rules = array(
+                'h_email'       => 'required|email|max:255',
+                'h_code'        => 'required|string|max:8',
+                'fname'         => 'required|string',
+                'mname'         => 'required|string',
+                'lname'         => 'required|string',
+                'sex'           => 'required|string',
+                'perm_address'  => 'required|string|min:10'
+            );
 
-        $validator = Validator::make($input, $rules);
-        if ($validator->fails()) {
-            $response['success'] = False;
-            $response['message'] = $validator->errors();
-            return response()
-                ->json($response)
-                ->setCallback($request->input('callback'));                   
-        }else{
-            $user_id = DB::table('users')
-                        ->select('id','email','activation_code as code')
-                        ->where('activation_code','=',$input['h_code'])
-                        ->orWhere('email','=',$input['h_email'])
-                        ->get();
-            $user = user_info::where('user_id',$user_id[0]->id)->first();
-            $user->first_name =  $input['fname'];
-            $user->middle_name = $input['mname'];
-            $user->last_name = $input['lname'];
-            $user->perm_address = $input['perm_address'];
-            if($user->sex !== $input['sex']) $user->sex = $input['sex'];
-            if($user->suffix_name !== $input['suffix_name']) $user->suffix_name = $input['suffix_name'];
-            if($user->tempo_address !== $input['tempo_address']) $user->tempo_address = $input['tempo_address'];
-            if($user->office_address !== $input['office_address']) $user->office_address = $input['office_address'];
-            
-            $user->updated_at = Carbon::now();
-            $user->save();
-        }
-        /*$redirect = 'auth/profile/edit/' . $input['h_code'];
-        return redirect()->intended($redirect);*/
-        
+            $validator = Validator::make($input, $rules);
+            if ($validator->fails()) {
+                $response['success'] = False;
+                $response['message'] = $validator->errors();
+                return response()
+                    ->json($response)
+                    ->setCallback($request->input('callback'));                   
+            }else{
+                $user_id = DB::table('users')
+                            ->select('id','email','activation_code as code')
+                            ->where('activation_code','=',$input['h_code'])
+                            ->orWhere('email','=',$input['h_email'])
+                            ->get();
+                $user = user_info::where('user_id',$user_id[0]->id)->first();
+                $user->first_name =  $input['fname'];
+                $user->middle_name = $input['mname'];
+                $user->last_name = $input['lname'];
+                $user->perm_address = $input['perm_address'];
+                if($user->sex !== $input['sex']) $user->sex = $input['sex'];
+                if($user->suffix_name !== $input['suffix_name']) $user->suffix_name = $input['suffix_name'];
+                if($user->tempo_address !== $input['tempo_address']) $user->tempo_address = $input['tempo_address'];
+                if($user->office_address !== $input['office_address']) $user->office_address = $input['office_address'];
+                
+                $user->updated_at = Carbon::now();
+                $user->save();
+            }
+            /*$redirect = 'auth/profile/edit/' . $input['h_code'];
+            return redirect()->intended($redirect);*/
+        }    
     }
     /**
      *  Log out the user 
