ed7c4afef0cb5c044e72d585ebb7431c2fd21586
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-07-02 04:23:31 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -133,7 +133,7 @@ Route::group(['middleware' => ['auth:sanctum']], function(){
 		//Route::get('/', [notesController::class,'getNote']);
 		Route::prefix('/list')->group(function () {
 			Route::post('/', [notesController::class,'getPatientNotes'])->name('noteList');	//ajax request
-			//Route::get('/', [notesController::class,'getPatientNotes']);
+			Route::get('/', [notesController::class,'getPatientNotes']);
 			Route::post('task', [notesController::class,'getTaskExerDataNotes'])->name('noteListTask');	//ajax request
 			//Route::get('task', [notesController::class,'getTaskExerDataNotes']);
 		});
