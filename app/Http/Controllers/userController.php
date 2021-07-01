89d6b6130e15796efffd7991759e30c232d6eb29
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-07-01 16:44:53 +0800

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -326,7 +326,7 @@ class userController extends Controller
                         $searchStr = strtoupper('%' . str_replace(' ', '%',$input['search']) . '%');
                         $query = DB::table('users_info')
                                     ->select('users_info.user_id as id')
-                                    ->where(DB::raw('UPPER(CONCAT(first_name, " ",middle_name," ", last_name))'), "LIKE", $searchStr )
+                                    ->where(DB::raw('UPPER(CONCAT(first_name, \" \",middle_name,\" \", last_name))'), "LIKE", $searchStr )
                                     ->where('developers.project_id','=',Auth::user()->projects[Auth::user()->user_types]->project_id)
                                     ->where('users_info.user_id','<>',intval(Auth::user()->id))
                                     ->leftjoin("developers","developers.user_id",'=','users_info.user_id')
