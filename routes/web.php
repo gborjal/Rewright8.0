4ff77073787e540683000264786eaa2c71febea5
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-07-02 13:49:30 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,7 @@ Route::middleware(['web'])->group(function () {
 	});
 	Route::get('/discussion/image/{image}', [imagesController::class,'discussionImage']);	//possible change
 });
-Route::middleware(['Auth:sanctum'])->group(function () {
+Route::middleware(['Auth:api'])->group(function () {
 	Route::post('/tasks/info', [taskController::class,'getTask'])->name('taskBoardInfo');	//ajax request
 		//Route::get('info', [taskController::class,'getTask']);
 	Route::post('/list/task', [notesController::class,'getTaskExerDataNotes'])->name('noteListTask');	//ajax request
