ae349164436e5f0fa6bee94f23c28dd0df907bac
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-07-03 18:08:20 +0800

    Update functionsPhysician.js

--- public/js/functionsPhysician.js
+++ public/js/functionsPhysician.js
@@ -101,10 +101,10 @@ function searchPatient(){
                         a.onclick = function(){ loadNotes(res_id) };
                         
                     var d = addNode(div2,"div","patientResThread"+result[0].user_id,undefined,"col s12",undefined,undefined,undefined);
-                    var d_t_d = addNode(d,"div",undefined,undefined,"modal-content container");
+                    var d_t_d = addNode(d,"div",undefined,undefined,"container");
                         addNode(d_t_d,"ul",undefined,undefined,"collection");
                     var t = addNode(div2,"div","patientResTask"+result[0].user_id,undefined,"col s12",undefined,undefined,undefined);
-                    var t_d = addNode(t,"div",undefined,undefined,"modal-content container");
+                    var t_d = addNode(t,"div",undefined,undefined,"container");
                         addNode(t_d,"ul",undefined,undefined,"collapsible popout");
                     var notes = addNode(div2,"div",undefined,undefined,"col s12",undefined,undefined,undefined);
                         addNode(notes,"ul","patientResNotes"+res_id,undefined,"collapsible popout");
