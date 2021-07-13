8fe7e3e3b5a30ee7e2662843ef4094400f5dbb6c
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-07-13 20:54:36 +0800

    Update notesController.php

--- app/Http/Controllers/notesController.php
+++ app/Http/Controllers/notesController.php
@@ -192,7 +192,8 @@ class notesController extends Controller
                     ->json($response)
                     ->setCallback($request->input('callback'));                   
             }else{
-            	if(!is_null($input['task_exer_data_id'])){
+            	if(array_key_exists('task_exer_data_id', $input) && !is_null($input['task_exer_data_id'])){
+            	//if(!is_null($input['task_exer_data_id'])){
             		$tmp = task_exer_data::where('id','=',intval($input['task_exer_data_id']))->first();
             		$input['patient_id'] = assigned_to::where('id','=',intval($tmp->task_assignment_id))->first()->user_id;
             	}
