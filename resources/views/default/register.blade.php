057188a44d07d1b9f7da2eb446f80c716f081f3d
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2022-06-25 16:46:47 +0800

    Update register.blade.php

--- resources/views/default/register.blade.php
+++ resources/views/default/register.blade.php
@@ -7,7 +7,7 @@
 			<div class="col s12 m6 l6 offset-m3 offset-l3">
 				<div id="f" class="card medium">
 					<ul class="tabs">
-						<li class="tab col s3"><a class="blue-text text-accent-4" href="#div_login" onclick="changeRegCard(0);">Login</a></li>
+						<li class="tab col s3"><a class="blue-text text-accent-4" href="#div_login" onclick="changeRegCard(0);">Register</a></li>
 						<div class="indicator blue darken-4" style="z-index:1"></div>
 					</ul>
 					<div id="div_login" class="col s12">
