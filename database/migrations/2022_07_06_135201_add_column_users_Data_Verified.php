4ebf126f4a954a0443a9186dfb7439526f20883b
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2022-07-11 18:23:25 +0800

    migration

--- database/migrations/2022_07_06_135201_add_column_users_Data_Verified.php
+++ database/migrations/2022_07_06_135201_add_column_users_Data_Verified.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnUsersDataEMailVerified extends Migration
+class AddColumnUsersDataVerified extends Migration
 {
     /**
      * Run the migrations.
