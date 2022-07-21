594edb6d0355203d28890891e788e8a6bd507efa
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2022-07-21 12:28:38 +0800

    Update dashboardAdmin.blade.php

--- resources/views/dashboardAdmin.blade.php
+++ resources/views/dashboardAdmin.blade.php
@@ -139,7 +139,7 @@
 										</div>
 									</div>
 									<div id="getActivationCode" class="col s12" style = "display:none;">
-										{!! Form::open(['route'=>'getCode','id'=>'f_activation']) !!}
+										{!! Form::open(['route'=>'getActivationCode','id'=>'f_activation']) !!}
 										{!! csrf_field() !!}
 											
 											{!! Form::email('email',null,['class'=>'form-control']) !!}
