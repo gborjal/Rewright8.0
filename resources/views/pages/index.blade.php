ba7455ae7f799a4bfd4a01cf5f879218e1f54d72
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2024-07-05 10:36:05 +0800

    Update index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -41,12 +41,12 @@
 								</a>
 							</li>
 							<li class="no padding">
-								<!--a href="register" data-link="{{ route('login_demo_phy') }}">
+								<!--a href="register" data-link="{{ //route('login_demo_phy') }}">
 									<i class="material-icons left">play_circle_outline</i>Demo as Physician 
 								</a-->
 							</li>
 							<li class="no padding">
-								<!--a href="register" data-link="{{ route('login_demo_patient') }}">
+								<!--a href="register" data-link="{{ //route('login_demo_patient') }}">
 									<i class="material-icons left">play_circle_outline</i>Demo as Patient 
 								</a-->
 							</li>
