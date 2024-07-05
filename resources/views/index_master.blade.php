ed7b2113e8700b593e796cbf24745f35e0679205
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2024-07-05 22:25:59 +0800

    update
    added footer

--- resources/views/index_master.blade.php
+++ resources/views/index_master.blade.php
@@ -27,7 +27,34 @@
             </div>
         </nav>
         @yield('content')
-        
+        <footer class="page-footer grey darken-3">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="light-blue-text text-darken-1">ReWrighT: Hand and Wrist rehabilitation system</h5>
+                <p class="light-blue-text text-darken-2">Made with Laravel</p>
+                <p class="light-blue-text text-darken-2">Made with LeapJS PlayBack</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="light-blue-text text-darken-1">Links</h5>
+                <ul>
+                  <li><a class="light-blue-text text-darken-2" href="https://github.com/leapmotion/leapjs-playback">LeapJS Playback</a></li>
+                  <li><a class="light-blue-text text-darken-2" href="https://laravel.com/">Laravel</a></li>
+                  <li><a class="light-blue-text text-darken-2" href="https://github.com/gborjal">Gabriel Luis Borjal</a></li>
+                  <li><a class="light-blue-text text-darken-2" href="#!">Ralph Deloria</a></li>
+                  <li><a class="light-blue-text text-darken-2" href="#!">Rheyvin Demerey</a></li>
+                  <li><a class="light-blue-text text-darken-2" href="#!">Dave Kristian Au</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container light-blue-text text-darken-1">
+            © 2021 ReWrighT: Hand and Wrist rehabilitation system
+            <!--a class="grey-text text-lighten-4 right" href="#!">More Links</a-->
+            </div>
+          </div>
+        </footer>
         <script type = "text/javascript" src = "{{ URL::asset('js/materialize.min.js') }}"/></script>
         <script type = "text/javascript" src = "{{ URL::asset('js/index_functions.js') }}"/></script>
         @yield('errors')
