6cd1bd59173298bfc9073aebf677c8d7a7e0fac6
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2024-07-05 16:56:22 +0800

    update
    cleaned fn login for physician

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -292,9 +292,6 @@ class AuthController extends Controller
         // create our user data for the authentication
         $userdata['login_id'] = "demo_physician@gmail.com";
         $userdata['password'] = "rewrightdemophys";
-
-        /*$userdata['login_id'] = "demo_patient_1@gmail.com";
-        $userdata['password'] = "1lw3yjuo";*/
         $remember = false;
 
         $field = filter_var($userdata['login_id'], FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
