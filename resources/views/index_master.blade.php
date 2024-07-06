c4c61ddb2bf96dd2eb2bb8db7c1256eee6ba067b
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2024-07-06 11:57:21 +0800

    Update index_master.blade.php
    added credit to the artwork used

--- resources/views/index_master.blade.php
+++ resources/views/index_master.blade.php
@@ -36,7 +36,14 @@
                 <p class="light-blue-text text-darken-2">Made with LeapJS PlayBack</p>
               </div>
               <div class="col l4 offset-l2 s12">
-                <h5 class="light-blue-text text-darken-1">Links</h5>
+                <h6 class="light-blue-text text-darken-1">Artwork</h6>
+                <ul>
+                  <li><a class="light-blue-text text-darken-1" href="https://www.facebook.com/faustfolio">Ms Jean Thompson</a></li>
+                  
+                </ul>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h6 class="light-blue-text text-darken-1">Links</h6>
                 <ul>
                   <li><a class="light-blue-text text-darken-2" href="https://github.com/leapmotion/leapjs-playback">LeapJS Playback</a></li>
                   <li><a class="light-blue-text text-darken-2" href="https://laravel.com/">Laravel</a></li>
