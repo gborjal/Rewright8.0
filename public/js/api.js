3ea3ea7dae46f17a6eafd44c7e480440cc02da5d
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2024-07-27 17:49:57 +0800

    update filterGenText function
    fix for missing discussion image that has no caption

--- public/js/api.js
+++ public/js/api.js
@@ -337,9 +337,11 @@ function filterGenText(text,image){
 							var text= ret.text;						
 							i = ret.pos;
 							t = imageCard(images[k],text);
+						}else{
+							t = imageCard(images[k]);
 						}
 					}else{
-						t= imageCard(images[k]);
+						t = imageCard(images[k]);
 					}
 					k++;
 					newText+=t;
