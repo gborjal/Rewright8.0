d46ecf2db3d52329fad59cdc15dd0055f425ede7
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2024-07-27 18:17:45 +0800

    Update userSeeder.php

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -19,7 +19,7 @@ class userSeeder extends Seeder
     public function run()
     {
         $table = 'users';
-        DB::table($table)->delete();
+        \DB::table($table)->delete();
 
         //admin
         $admin_id = \DB::table($table)->insertGetId([
@@ -29,7 +29,7 @@ class userSeeder extends Seeder
             'activation_code' => '59626288',
             'user_types'=> 0
         ]);
-        DB::table('users_info')->insert([
+        \DB::table('users_info')->insert([
             "user_id"=>$admin_id,
             "profile"=>'1_profile_1.jpg',
             "banner"=>'1_banner_1.jpg',
@@ -49,7 +49,7 @@ class userSeeder extends Seeder
             'size' => 1000,
             'active' => True
         ]);
-        DB::table('developers')->insert([
+        \DB::table('developers')->insert([
             'project_id'    => $admin_project_id,
             'user_id'       => $admin_id,
             'role'          => 0
@@ -90,7 +90,7 @@ class userSeeder extends Seeder
             'activation_code' => 'sy16dl3l',
             'user_types'=> 1
         ]);
-        DB::table('users_info')->insert([
+        \DB::table('users_info')->insert([
             "user_id"=>$physician_id,
             "profile"=>'1_profile_1.jpg',
             "banner"=>'1_banner_1.jpg',
@@ -112,12 +112,12 @@ class userSeeder extends Seeder
             'active' => True
         ]);
         //PT joins admin's group
-        DB::table('developers')->insert([
+        \DB::table('developers')->insert([
             'project_id'    => $admin_project_id,
             'user_id'       => $physician_id,
             'role'          => 1
         ]);
-        DB::table('developers')->insert([
+        \DB::table('developers')->insert([
             'project_id'    => $physician_project_id,
             'user_id'       => $physician_id,
             'role'          => 1
@@ -163,7 +163,7 @@ class userSeeder extends Seeder
             'activation_code' => 'cbzbtuy7',
             'user_types'=> 2
         ]);
-        DB::table('users_info')->insert([
+        \DB::table('users_info')->insert([
             "user_id"=>$patient_id,
             "profile"=>'1_profile_1.jpg',
             "banner"=>'1_banner_1.jpg',
@@ -177,7 +177,7 @@ class userSeeder extends Seeder
             "office_address"=>NULL
         ]);
 
-        DB::table('developers')->insert([
+        \DB::table('developers')->insert([
             'project_id'    => $physician_project_id,
             'user_id'       => $patient_id,
             'role'          => 2
